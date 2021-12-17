f7d603b585847f543a1b92e0ce1732b4faf23477
Vic <vfacory@gmail.com>
2021-12-17 22:15:45 +0000

    Added "Burglary series19 prevent changes" solution

---
+++ 1)Very Easy/burglary-series-19-prevent-changes.js
@@ -0,0 +1,10 @@
+function preventChanges(obj) {
+    // write your code here
+      // don't use a return statement
+      Object.freeze(obj);	
+      
+    // DON'T CHANGE OR REMOVE THE LINES BELOW
+    obj.noChanges = false;
+    obj.signature = "whatever";
+    return obj;
+  }
