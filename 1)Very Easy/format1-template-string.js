367757599036dc55ea1deb3a13e565599199c72f
Victoria Cory <vfacory@gmail.com>
2022-01-07 22:15:09 +0000

    Added "Format I - Template string" solution

---
+++ 1)Very Easy/format1-template-string.js
@@ -0,0 +1,6 @@
+// modify the template variable to be a template string 
+function format(a, b, c) {
+// the result string must give: "Their names were: a, b and c."
+	const template = `Their names were: ${a}, ${b} and ${c}.`;
+	return template
+}
