456b7585bed13ca28e006f2360e5da48a5a1bca5
Victoria Cory <vfacory@gmail.com>
2022-01-24 19:15:04 +0000

    Added "Luke, I am your" solution

---
+++ 1)Very Easy/luke-i-am-your.js
@@ -0,0 +1,6 @@
+function relationToLuke(name) {
+	return name === "Darth Vader" ? "Luke, I am your father."
+	: name === "Leia" ? "Luke, I am your sister."
+	: name === "Han" ? "Luke, I am your brother in law."
+	: "Luke, I am your droid.";
+}
