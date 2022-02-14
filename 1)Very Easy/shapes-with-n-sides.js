54bb4b28a6d731f7b69798b905c2f17ea46aadcb
Victoria Cory <vfacory@gmail.com>
2022-02-14 23:03:45 +0000

    Added "Shapes with N sides" solution

---
+++ 1)Very Easy/shapes-with-n-sides.js
@@ -0,0 +1,4 @@
+function nSidedShape(n) {
+  let shape = ['circle', 'semi-circle', 'triangle', 'square', 'pentagon', 'hexagon', 'heptagon', 'octagon', 'nonagon', 'decagon']
+  return shape [n-1];
+}
