f96e6c8b33f254e5e923fd95fc51e77dc8eb465a
Victoria Cory <vfacory@gmail.com>
2022-02-26 21:32:31 +0000

    Added "Filter strings from array" solution
    Accidentally committed before finishing title. Added tests.

--- 1)Very Easy/filter-strings-from-array.js
+++ 1)Very Easy/filter-strings-from-array.js
@@ -1,3 +1,9 @@
 function filterArray(arr) {
 	return arr.filter (x => Number.isInteger(x))
 }
+
+
+Test.assertSimilar(filterArray([1, 2, 3, "a", "b", 4]), [1, 2, 3, 4])
+Test.assertSimilar(filterArray(["A", 1, "&amp", 0, -9, "Edabit"]), [1, 0, -9])
+Test.assertSimilar(filterArray(["Nothing", "here"]),[])
+Test.assertSimilar(filterArray([1, 2, 3, 3.5 , "a", "b", 4 , 5.8 , 6]), [1, 2, 3, 4 , 6])
