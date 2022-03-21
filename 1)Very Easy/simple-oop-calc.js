50fcaa7f2b4f3a9ca0ab4b7fac73e0e932b1fa09
Victoria Cory <vfacory@gmail.com>
2022-03-21 11:57:50 +0000

    Added "Simple OOP Calculator" solution

---
+++ 1)Very Easy/simple-oop-calc.js
@@ -0,0 +1,19 @@
+class Calculator {
+	// Write functions to add(), subtract(), multiply() and divide()
+	
+	add(a,b) {
+		return a + b;
+	}
+	
+	subtract(a,b) {
+		return a - b;
+	}
+	
+	multiply(a,b) {
+		return a * b;
+	}
+	
+	divide(a,b) {
+		return a / b;
+	}
+}
