e11ff7237c430616e1a2aa34ec18dcdcea397fda
Victoria Cory <vfacory@gmail.com>
2022-04-06 12:11:36 +0100

    Added "Find the other two sides" solution

--- 1)Very Easy/find-the-other-two-sides.js
+++ 1)Very Easy/find-the-other-two-sides.js
@@ -1,3 +1,9 @@
 function otherSides(length) {
 	return [length*2, + ((length * Math.sqrt(3)).toFixed(2))]	
 }
+
+
+Test.assertSimilar(otherSides(1), [2, 1.73])
+Test.assertSimilar(otherSides(2), [4, 3.46])
+Test.assertSimilar(otherSides(3), [6, 5.2])
+Test.assertSimilar(otherSides(7), [14, 12.12])
