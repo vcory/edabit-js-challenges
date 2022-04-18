1e9bfa93c632a713c18bfb2ef0f4cbbae69de8dc
Victoria Cory <vfacory@gmail.com>
2022-04-18 18:57:20 +0100

    Added "Sum of cubes" solution

--- 2) Easy/sum-of-cubes.js
+++ 2) Easy/sum-of-cubes.js
@@ -1,4 +1,4 @@
 function sumOfCubes(nums) {
 	return nums.length === 0 ? 0 : nums.map(num =>
-		Math.pow(num, 3)).reduce((acc, currentVal) => currentVal + acc)
+		Math.pow(num, 3)).reduce((acc, currentVal) => currentVal + acc);
 }
